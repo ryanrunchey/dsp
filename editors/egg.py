1f27315775dbc90a7fe80e6abcc74070d6d39bd2
Ryan Runchey <ryanrunchey@users.noreply.github.com>
2017-12-11 22:41:44 -0800

    Add files via upload

---
+++ editors/egg.py
@@ -0,0 +1,2 @@
+print("I'm adding a couple of lines using  emacs.")
+print("I'm saving a file named 'egg.py' to my desktop.")
---
+++ editors/vinegar.py
@@ -0,0 +1,6 @@
+print("This file was created using the vi editor")
+print("This file was created by Reshama")
+print("I can use both Command and Insert modes.")
+print("I can enter Insert mode by hitting the i key.")
+print("I can escape Insert mode by hitting the Esc button.")
+
